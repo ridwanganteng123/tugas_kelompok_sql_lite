93c782354c009276bf93fd65d93b985679f1ef85
Sabihis <ridwanganteng478@gmail.com>
2019-09-11 09:28:39 +0700

    .

--- app/src/main/java/com/example/tugas_kelompok_sql_lite/MainActivity.java
+++ app/src/main/java/com/example/tugas_kelompok_sql_lite/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.tugas_kelompok_sql_lite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int waktu_loading = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(home);
+                finish();
+            }
+    },waktu_loading);
+}
 }
